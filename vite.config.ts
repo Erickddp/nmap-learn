2b3f56378970b85ac9853845345831b57b4374d2
Erick D <erickddp620@gmail.com>
2025-11-27 18:21:49 -0600

    Build hacia docs para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/nmap-learn/',
+  build: {
+    outDir: 'docs',   // 👈 aquí está la magia
+  },
   plugins: [react()],
 });
